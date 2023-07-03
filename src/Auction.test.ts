37eb09731d205f6698e18d734422fcf59f303372
Rute Figueiredo <rfigueiredo.dev@gmail.com>
2023-07-03 14:23:22 +0100

    testing init for zkProgram

--- src/Auction.test.ts
+++ src/Auction.test.ts
@@ -1,7 +1,21 @@
-// import { Auction } from './Auction';
+import { AccountUpdate, Field, Mina, PrivateKey, PublicKey } from 'snarkyjs';
+import { Auction } from './Auction';
 
-describe('Auction.js', () => {
-  describe('Auction()', () => {
-    it.todo('should be correct');
+describe('Auction', () => {
+  beforeAll(async () => {
+    await Auction.compile();
+  });
+
+  it('should create a new Auction', async () => {
+    const initialProof = await Auction.init();
+
+    expect(initialProof.publicOutput.biddsTreeRoot).toStrictEqual(Field(0));
+    expect(initialProof.publicOutput.nullifierMapRoot).toStrictEqual(Field(0));
+    expect(initialProof.publicOutput.lastValidBid.amount).toStrictEqual(
+      Field(0)
+    );
+    expect(initialProof.publicOutput.lastValidBid.bidder).toStrictEqual(
+      PublicKey.empty()
+    );
   });
 });
--- src/Auction.ts
+++ src/Auction.ts
@@ -1,3 +1,39 @@
-import { SmartContract } from 'snarkyjs';
+import {
+  Experimental,
+  Field,
+  MerkleMap,
+  PublicKey,
+  SmartContract,
+  Struct,
+} from 'snarkyjs';
 
-export class Auction extends SmartContract {}
+export class Bid extends Struct({
+  amount: Field,
+  bidder: PublicKey,
+}) {}
+
+export class AuctionState extends Struct({
+  lastValidBid: Bid,
+  biddsTreeRoot: Field,
+  nullifierMapRoot: Field,
+}) {}
+
+export const Auction = Experimental.ZkProgram({
+  publicOutput: AuctionState,
+
+  methods: {
+    init: {
+      privateInputs: [],
+      method() {
+        return new AuctionState({
+          lastValidBid: new Bid({
+            amount: Field(0),
+            bidder: PublicKey.empty(),
+          }),
+          biddsTreeRoot: Field(0),
+          nullifierMapRoot: Field(0),
+        });
+      },
+    },
+  },
+});
